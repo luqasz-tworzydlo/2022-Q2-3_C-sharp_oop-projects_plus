c15b65be6f9024e3a6e6cc9e002dbf5851d59a37
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-05-09 23:57:43 +0200

    Add files via upload

---
+++ 1_oop-kz1-osoba/src/oop-kz1-osoba/oop-kz1-osoba/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_kz1_osoba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////
+            //
+            // Łukasz Tworzydło - nr albumu: gd29623 [projekt 1]
+            //
+            //////////////////////////////////////////////////
+            Console.WriteLine("\n(1) Z1 => Wyświetlenie łącznie oraz osobno\n" +
+                "imienia oraz nazwiska, jak i wypisanie wieku\n" +
+                "[obliczonych lat za pomocą instrukcji w foreach] ;>\n");
+
+            kz1_osoba
+                nowa_osoba = new kz1_osoba("Missie", "Anaina");
+
+            DateTime DataUrodzenia = new DateTime(2012, 3, 18);
+            DateTime[] DataŚmierciNieśmierci =
+            {
+                new DateTime(2012, 3, 18),
+                DateTime.Today,
+                DateTime.Today.AddYears(1),
+                DateTime.Today.AddYears(3),
+                DateTime.Today.AddYears(7),
+            };
+
+            Console.WriteLine("Poznajcie " + nowa_osoba.Imię
+                + ", której nazwisko \n"
+                + "brzmi następująco: " + nowa_osoba.Jakieś_Nazwisko
+                + "!" + " :>>>");
+
+            Console.WriteLine("\nPełny zapis jej imienia i nazwiska"
+                + "\nwygląda następująco ;>\n=> " + nowa_osoba.ImięNazwisko + " :>>>");
+
+            Console.WriteLine("\nPoniżej jest wyświetlona ilość lat,\n" +
+                "jakie miała " + nowa_osoba.Imię + " w roku 2012,\n" +
+                "ile ma dzisiaj lat, ile będzie mieć za rok\n" +
+                "ile będzie mieć za 3 lata oraz ile za 7 lat :>");
+
+            foreach (DateTime wartości_dat in DataŚmierciNieśmierci)
+            {
+                TimeSpan nowa_wartość = wartości_dat - DataUrodzenia;
+                int years = (((int)nowa_wartość.TotalDays) / 365);
+                string dane_wyjściowe;
+                if (DataUrodzenia.AddYears(years) <= wartości_dat)
+                {
+                    dane_wyjściowe = string.Format("=> {0} lat/a", years);
+                    Console.WriteLine(dane_wyjściowe);
+                }
+                else
+                {
+                    dane_wyjściowe = string.Format("=> {0} lat/a", years - 1);
+                    Console.WriteLine(dane_wyjściowe);
+                }
+            }
+
+            //////////////////////////////////////////////////
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1_oop-kz1-osoba/src/oop-kz1-osoba/oop-kz1-osoba/kz1-osoba.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_kz1_osoba
+{
+    class kz1_osoba
+    {
+        string imię;
+        public string Nazwisko;
+        string imięNazwisko; // wartość do konstruktora ImięNazwisko
+
+        public string Imię // instrukcja [konstruktor] deklarująca imię
+        {
+            get
+            {
+                return imię;
+            }
+            set
+            {
+                imię = value;
+                sklejoneImięNazwisko();
+            }
+        }
+        public string Jakieś_Nazwisko // instrukcja [konstruktor] deklarująca nazwisko
+        {
+            get
+            {
+                return Nazwisko;
+            }
+            set
+            {
+                Nazwisko = value;
+                sklejoneImięNazwisko();
+            }
+        }
+        public string ImięNazwisko
+        {
+            get
+            {
+                return imięNazwisko;
+            }
+            set
+            {
+                string[] rozbicie = value.Split(' ');
+                imię = rozbicie[0];
+                if (rozbicie.Length > 1)
+                    Nazwisko = rozbicie[0];
+                else
+                    Nazwisko = "";
+                sklejoneImięNazwisko();
+            }
+        }
+        void sklejoneImięNazwisko()
+        {
+            if (Nazwisko != " ")
+                imięNazwisko = imię + " " + Nazwisko;
+            else
+                imięNazwisko = imię;
+        }
+
+        public kz1_osoba(string imię, string Nazwisko)
+        {
+            this.imię = imię;
+            this.Nazwisko = Nazwisko;
+            sklejoneImięNazwisko();
+        }
+    }
+}
