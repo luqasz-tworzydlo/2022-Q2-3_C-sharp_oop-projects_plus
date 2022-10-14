21d7f95040978c4e7b3321d4ed7af3440b9d9abf
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-10-14 09:57:32 +0200

    Add files via upload

---
+++ 3_oop-kz3-wektor/src/oop-kz3-wektor/oop-kz3-wektor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_kz3_wektor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////
+            //
+            // Łukasz Tworzydło - nr albumu: gd29623 [projekt 3]
+            //
+            //////////////////////////////////////////////////
+            Console.WriteLine("\n(3) Z3 => Operacje na wektorach ;>");
+
+            Test_Wektora_Ogólnego();
+
+            Console.WriteLine("HI");
+
+            //////////////////////////////////////////////////
+
+            Console.ReadKey();
+        }
+        static void Test_Wektora_Ogólnego()
+        {
+            Wektor
+                v = new Wektor(1, 2),
+                w = new Wektor(3, 4),
+                ss = Wektor.Suma(v, w);
+
+            Console.WriteLine("Iloczyn skalarny dla dwóch wektorów, czyli:\n" +
+                "v o wymiarze (" + v.Wymiar + ") oraz dla w o wymiarze (" + w.Wymiar + ") wynosi: ");
+            Console.WriteLine(
+                $"v = {Wektor.IloczynSkalarny(v, w)}");
+
+            Console.WriteLine("Po dodaniu dwóch wektorów ich rozmiar wygląda następująco: \n" +
+                $"|ss| = {ss.Długość}");
+
+            Console.WriteLine("Natomiast iloczyn dla ss (czyli sumy dwóch wektorów v i w) oraz wektora w wynosi: ");
+            Console.WriteLine(
+                $"w = {Wektor.IloczynSkalarny(ss, w)};");
+
+        }
+
+    }
+}
---
+++ 3_oop-kz3-wektor/src/oop-kz3-wektor/oop-kz3-wektor/kz3-wektor.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_kz3_wektor
+{
+    internal class Wektor
+    {
+        readonly double[] współrzędne;
+        public double this[byte indeks]
+        {
+            get { return współrzędne[indeks]; }
+            set
+            {
+                switch (value)
+                {
+                    case double.NaN:
+                    case double.PositiveInfinity:
+                    case double.NegativeInfinity:
+                        throw new Exception(
+                            $"Niepoprawna wartość {value} na współrzędnej {indeks} wektora");
+                    default:
+                        współrzędne[indeks] = value;
+                        break;
+                }
+            }
+        }
+        public byte Wymiar
+        {
+            get { return (byte)współrzędne.Length; }
+        }
+        public double Długość
+        {
+            get
+            {
+                return Math.Sqrt(
+                    IloczynSkalarny(this, this)
+                    );
+            }
+        }
+        public Wektor(byte wymiar)
+        {
+            współrzędne = new double[wymiar];
+        }
+        public Wektor(params double[] współrzędne)
+        {
+            this.współrzędne = (double[])współrzędne.Clone();
+        }
+        public static double IloczynSkalarny(
+            Wektor v,
+            Wektor w)
+        {
+            if (v.Wymiar != w.Wymiar)
+                return double.NaN;
+            double suma = 0;
+            for (byte indeks = 0; indeks < v.Wymiar; indeks++)
+                suma += v.współrzędne[indeks] * w.współrzędne[indeks];
+                // suma += v[indeks] * w[indeks];
+            return suma;
+        }
+        public static Wektor Suma(
+            Wektor v,
+            Wektor w)
+        {
+            if (v.Wymiar != w.Wymiar)
+                throw new Exception(
+                    $"Niezgodne wymiary sumowanych wektorów {v.Wymiar} i {w.Wymiar}");
+
+            Wektor nowy = new Wektor(v.Wymiar);
+            for (byte indeks = 0; indeks < v.Wymiar; indeks++)
+                nowy.współrzędne[indeks] = v.współrzędne[indeks] + w.współrzędne[indeks];
+                // nowy[indeks] = v[indeks] + w[indeks];
+
+            return nowy;
+        }
+    }
+}
