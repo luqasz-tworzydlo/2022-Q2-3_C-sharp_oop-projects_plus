8622d341444323583075d0495625cc5868fcd604
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-10-16 17:04:03 +0200

    Add files via upload

--- 3_oop-kz3-wektor/src/oop-kz3-wektor/oop-kz3-wektor/Program.cs
+++ 3_oop-kz3-wektor/src/oop-kz3-wektor/oop-kz3-wektor/Program.cs
@@ -15,36 +15,76 @@ namespace oop_kz3_wektor
             // Łukasz Tworzydło - nr albumu: gd29623 [projekt 3]
             //
             //////////////////////////////////////////////////
-            Console.WriteLine("\n(3) Z3 => Operacje na wektorach ;>");
+            Console.WriteLine("\n(3.1) Z3 => Operacje na wektorach ;>" +
+                "\n[ wymiar wektora, długość wektora,\nsuma wektorów, iloczyn skalarny wektorów ]\n");
 
-            Test_Wektora_Ogólnego();
+            Wektor_Wymiary();
 
-            Console.WriteLine("HI");
+            Console.WriteLine("\n/// /// /// /// /// /// /// /// /// /// /// ///");
+
+            Console.WriteLine("\n(3.2) Z3 => Operacje na wektorach ;>" +
+                "\n[ dodawanie wektorów, odejmowanie wektorów,\nmnożenie wektorów, dzielenie wektorów ]");
+
+            Operacje_Wektorów();
 
             //////////////////////////////////////////////////
 
             Console.ReadKey();
         }
-        static void Test_Wektora_Ogólnego()
+        static void Wektor_Wymiary()
         {
             Wektor
-                v = new Wektor(1, 2),
-                w = new Wektor(3, 4),
-                ss = Wektor.Suma(v, w);
+                v = new Wektor(1, 3),
+                w = new Wektor(5, 7),
+                s = Wektor.Suma(v, w);
 
-            Console.WriteLine("Iloczyn skalarny dla dwóch wektorów, czyli:\n" +
-                "v o wymiarze (" + v.Wymiar + ") oraz dla w o wymiarze (" + w.Wymiar + ") wynosi: ");
-            Console.WriteLine(
-                $"v = {Wektor.IloczynSkalarny(v, w)}");
+            Console.WriteLine("Nasze wektory, na których będziemy\nwykonywać różne operacje to \"v\" oraz \"w\" :>\n");
 
-            Console.WriteLine("Po dodaniu dwóch wektorów ich rozmiar wygląda następująco: \n" +
-                $"|ss| = {ss.Długość}");
+            Console.WriteLine("Iloczyn skalarny dla naszych dwóch wektorów, czyli:\n" +
+                "v o wymiarze (" + v.Wymiar + ") oraz dla w o wymiarze (" + w.Wymiar + ") wynosi: " +
+                $"v = {Wektor.IloczynSkalarny(v, w)}\n");
 
-            Console.WriteLine("Natomiast iloczyn dla ss (czyli sumy dwóch wektorów v i w) oraz wektora w wynosi: ");
-            Console.WriteLine(
-                $"w = {Wektor.IloczynSkalarny(ss, w)};");
+            Console.WriteLine("Po dodaniu dwóch wektorów ich rozmiar wygląda\nnastępująco: " +
+                $"|s| = {s.Długość}\n");
 
+            Console.WriteLine("Natomiast iloczyn dla s (czyli sumy dwóch\nwektorów v i w) oraz wektora w wynosi: " + 
+                $"w = {Wektor.IloczynSkalarny(s, w)};");
         }
+        static void Operacje_Wektorów()
+        {
+            Wektor
+                v = new Wektor() { X = 4, Y = -4 },
+                w = new Wektor() { X = -9, Y = 9 },
+                s = v + w, // doddawanie wektorów
+                v2 = v * 2, // mnożenie przez skalar
+                v3 = 3 * v, // mnożenie przez skalar
+                m1 = -w, // odwrócenie wartości
+                m2 = -v, // odwrócenie wartości
+                r = v - w, // odejmowanie wektorów
+                vd2 = v / 2, // dzielenie przez skalar
+                vd4 = 4 / v, // dzielenie przez skalar
+                wd3 = w / 3, // dzielenie przez skalar
+                wd9 = 9 / w // dzielenie przez skalar
+                ;
 
+            Console.WriteLine("\nKolejne, inne operacje na wektorach :>");
+
+            Console.WriteLine("\nNasz pierwszy wektor początkowy to: " + $"v = {v}");
+            Console.WriteLine("\nZ kolei nasz drugi wektor początkowy to: " + $"w = {w}");
+
+            Console.WriteLine("\n<dodawanie> => nowy wektor po wykonaniu operacji dodawania: " + $"v+w = {s}");
+
+            Console.WriteLine("\n=> nasz nowy operator po odwróceniu wartości w wynosi: " + $"-w = {m1}");
+            Console.WriteLine("=> nasz nowy operator po odwróceniu wartości v wynosi: " + $"-v = {m2}");
+            Console.WriteLine("\n<odejmowanie> => nasz nowy operator po odjęciu dwóch wektorów wynosi: " + $"v-w = {r}");
+
+            Console.WriteLine("\n<mnożenie> => po pierwszej operacji mnożenia nowy wektor wynosi: " + $"v*2 = {v2}");
+            Console.WriteLine("<mnożenie> => po drugiej operacji mnożenia nowy wektor wynosi: " + $"3*v = {v3}");
+
+            Console.WriteLine("\n<dzielenie> => po pierwszej operacji dzielenia nowy wektor wynosi: " + $"v/2 = {vd2}");
+            Console.WriteLine("<dzielenie> => po drugiej operacji dzielenia nowy wektor wynosi: " + $"4/v = {vd4}");
+            Console.WriteLine("<dzielenie> => po trzeciej operacji dzielenia nowy wektor wynosi: " + $"w/3 = {wd3}");
+            Console.WriteLine("<dzielenie> => po czwartej operacji dzielenia nowy wektor wynosi: " + $"9/w = {wd9}");
+        }
     }
 }
--- 3_oop-kz3-wektor/src/oop-kz3-wektor/oop-kz3-wektor/kz3-wektor.cs
+++ 3_oop-kz3-wektor/src/oop-kz3-wektor/oop-kz3-wektor/kz3-wektor.cs
@@ -27,42 +27,34 @@ namespace oop_kz3_wektor
                 }
             }
         }
-        public byte Wymiar
+        public byte Wymiar // zwracanie wartości wymiaru wektora
         {
             get { return (byte)współrzędne.Length; }
         }
-        public double Długość
+        public double Długość // zwracanie wartości długości wektora
         {
-            get
-            {
-                return Math.Sqrt(
-                    IloczynSkalarny(this, this)
-                    );
-            }
+            get { return Math.Sqrt( IloczynSkalarny(this, this) ); }
         }
-        public Wektor(byte wymiar)
+        public Wektor(byte wymiar) // konstruktor nr 1 [może być zastąpiony w całości przez konstruktor nr 2]
         {
             współrzędne = new double[wymiar];
         }
-        public Wektor(params double[] współrzędne)
+        public Wektor(params double[] współrzędne) // konstruktor nr 2 [może być zastąpiony w całości przez konstruktor nr 1]
         {
             this.współrzędne = (double[])współrzędne.Clone();
         }
-        public static double IloczynSkalarny(
-            Wektor v,
-            Wektor w)
+        public static double IloczynSkalarny(Wektor v, Wektor w) // obliczanie iloczynu skalarnego wektorów
         {
             if (v.Wymiar != w.Wymiar)
                 return double.NaN;
             double suma = 0;
+
             for (byte indeks = 0; indeks < v.Wymiar; indeks++)
                 suma += v.współrzędne[indeks] * w.współrzędne[indeks];
-                // suma += v[indeks] * w[indeks];
+
             return suma;
         }
-        public static Wektor Suma(
-            Wektor v,
-            Wektor w)
+        public static Wektor Suma(Wektor v, Wektor w) // obliczanie sumy wektorów
         {
             if (v.Wymiar != w.Wymiar)
                 throw new Exception(
@@ -71,9 +63,69 @@ namespace oop_kz3_wektor
             Wektor nowy = new Wektor(v.Wymiar);
             for (byte indeks = 0; indeks < v.Wymiar; indeks++)
                 nowy.współrzędne[indeks] = v.współrzędne[indeks] + w.współrzędne[indeks];
-                // nowy[indeks] = v[indeks] + w[indeks];
 
             return nowy;
+        }
+
+        public double
+            X = 0,
+            Y = 0;
+
+        // poniżej jest operator zwracający nową instancję, będącą sumą wektorów
+        static public Wektor operator +(Wektor v, Wektor w)
+        {
+            return new Wektor()
+            {
+                X = v.X + w.X,
+                Y = v.Y + w.Y
+            };
+        }
+
+        // poniżej jest operator zwracający nową instancję, będącą wektorem wymnożonym przez skalar [1-2]
+        static public Wektor operator *(Wektor v, double s)
+        {
+            return new Wektor()
+            {
+                X = v.X * s,
+                Y = v.Y * s
+            };
+        }
+        // poniżej jest operator zwracający nową instancję, będącą wektorem wymnożonym przez skalar [2-2]
+        static public Wektor operator *(double s, Wektor v)
+        {
+            return v * s;
+        }
+
+        // poniżej jest operator zwracający nową instację, będącą odwróconą wartością
+        static public Wektor operator -(Wektor wektor)
+        {
+            return wektor * (-1);
+        }
+
+        // poniżej jest operator zwracający nową instancję, będącą różnicą wektorów
+        static public Wektor operator -(Wektor v, Wektor w)
+        {
+            return v + -w;
+        }
+
+        // poniżej jest operator zwracający nową instancję, będącą wektorem podzielonym przez skalar [1-2]
+        static public Wektor operator /(Wektor v, double s)
+        {
+            return new Wektor()
+            {
+                X = v.X / s,
+                Y = v.Y / s
+            };
+        }
+        // poniżej jest operator zwracający nową instancję, będącą wektorem podzielonym przez skalar [2-2]
+        static public Wektor operator /(double s, Wektor v)
+        {
+            return v / s;
+        }
+
+        public override string ToString()
+        {
+            return $"({X};{Y})";
         }
     }
 }
