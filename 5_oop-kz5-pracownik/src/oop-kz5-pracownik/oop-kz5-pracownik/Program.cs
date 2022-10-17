63a037fd401d5714a6bc679c343b023303f7bee7
luqasz-tworzydlo <72106587+luqasz-tworzydlo@users.noreply.github.com>
2022-10-17 08:31:52 +0200

    Add files via upload

---
+++ 5_oop-kz5-pracownik/src/oop-kz5-pracownik/oop-kz5-pracownik/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_kz5_pracownik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //////////////////////////////////////////////////
+            //
+            // Łukasz Tworzydło - nr albumu: gd29623 [projekt 5]
+            //
+            //////////////////////////////////////////////////
+            Console.WriteLine("\n(5) Z5 => Operacje na typach Pracownik oraz ZbiórPodwładnyhch ;>\n");
+
+            Console.WriteLine("/ / / = A = / / /\n");
+            Pracownicy_Podwładni_1();
+
+            Console.WriteLine("\n/ / / = B = / / /\n");
+            Pracownicy_Podwładni_2();
+
+            //////////////////////////////////////////////////
+
+            Console.ReadKey();
+        }
+        static void Pracownicy_Podwładni_1()
+        {
+            Pracownik
+                z1 = new Pracownik() { Imię = "Wojciech", Nazwisko = "Angel" },
+                z2 = new Pracownik() { Imię = "Aleksander", Nazwisko = "Toarski", BezpośredniPrzełożony = z1 },
+                z3 = new Pracownik() { Imię = "Miriana", Nazwisko = "Rominska"}
+                ;
+            z1.Podwładni.Add(z3);
+
+            Console.WriteLine(
+                $"=> Pracownik {z2} ma następującego przełożonego: {z2.BezpośredniPrzełożony}");
+            Console.WriteLine(
+                $"=> Pracownik {z3} ma w tymże przypadku przełożonego: {z3.BezpośredniPrzełożony}\n");
+            foreach (Pracownik pracownik in z1.Podwładni.Zbiór)
+                Console.WriteLine(
+                    $"=> Natomiast przełożony, czyli {z1} ma pracownika: {pracownik}"
+                    );
+        }
+        static void Pracownicy_Podwładni_2()
+        {
+
+            Pracownik
+                bp1 = new Pracownik() { Imię = "Wojciech", Nazwisko = "Angel" },
+                bp2 = new Pracownik() { Imię = "Renata", Nazwisko = "Light" };
+            Pracownik
+                p1 = new Pracownik() { Imię = "Martyna", Nazwisko = "Fiołkowska", BezpośredniPrzełożony = bp1 },
+                p2 = new Pracownik() { Imię = "Filip", Nazwisko = "Malinsky", BezpośredniPrzełożony = bp2 },
+                p3 = new Pracownik() { Imię = "Sebastian", Nazwisko = "Jankosk" },
+                p4 = new Pracownik() { Imię = "Lucine", Nazwisko = "Dehlas" };
+
+            bp2.Podwładni.Add(p3);
+
+            Console.WriteLine("W pierwszym kwartale 2022 roku sytuacja przełożonych i podwładnych wyglądała następująco:");
+
+            foreach (Pracownik pracownik in bp1.Podwładni.Zbiór)
+                Console.WriteLine(
+                    $"=> Przełożony, czyli {bp1} miał/a następującego podwładnego pracownika: {pracownik}");
+            foreach (Pracownik pracownik in bp2.Podwładni.Zbiór)
+                Console.WriteLine(
+                    $"=> Przełożona, czyli {bp2} miał/a następującego podwładnego pracownika: {pracownik}");
+
+            bp2.Podwładni.Remove(p3);
+            bp1.Podwładni.Add(p3);
+            bp1.Podwładni.Add(p4);
+
+            Console.WriteLine("\nJednakże po pewnych zmianach, jakie nastąpiły w drugim kwartale 2022 roku, obecnie ma następującą formę:");
+
+            foreach (Pracownik pracownik in bp1.Podwładni.Zbiór)
+                Console.WriteLine(
+                    $"=> Przełożony, czyli {bp1} ma następującego podwładnego pracownika: {pracownik}");
+            foreach (Pracownik pracownik in bp2.Podwładni.Zbiór)
+                Console.WriteLine(
+                    $"=> Przełożona, czyli {bp2} ma następującego podwładnego pracownika: {pracownik}");
+
+            Console.WriteLine("\nPodsumowując, od strony pracowników obecnie ich sytuacja wyglądała następująco");
+            Console.WriteLine(
+                $"<1> {p1} ma następującego przełożonego: {p1.BezpośredniPrzełożony}");
+            Console.WriteLine(
+                $"<2> {p2} ma następującego przełożonego: {p2.BezpośredniPrzełożony}");
+            Console.WriteLine(
+                $"<3> {p3} ma następującego przełożonego: {p3.BezpośredniPrzełożony}");
+            Console.WriteLine(
+                $"<4> {p4} ma następującego przełożonego: {p4.BezpośredniPrzełożony}");
+        }
+    }
+}
---
+++ 5_oop-kz5-pracownik/src/oop-kz5-pracownik/oop-kz5-pracownik/kz5-pracownik.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_kz5_pracownik
+{
+    internal class Pracownik
+    {
+        public class ZbiórPodwładnych : ISet<Pracownik>
+        {
+            Pracownik przełożony;
+            HashSet<Pracownik> zbiór = new HashSet<Pracownik>();
+            public HashSet<Pracownik> Zbiór
+            {
+                get => new HashSet<Pracownik>(zbiór);
+            }
+
+            public int Count => ((ICollection<Pracownik>)Zbiór).Count;
+
+            public bool IsReadOnly => ((ICollection<Pracownik>)Zbiór).IsReadOnly;
+
+            public void Add(Pracownik nowy)
+            {
+                if (zbiór.Contains(nowy))
+                    ;
+                if (nowy.przełożony != null)
+                    nowy.przełożony.Podwładni.Remove(nowy);
+                zbiór.Add(nowy);
+                nowy.przełożony = przełożony;
+            }
+            public bool Remove(Pracownik pracownik)
+            {
+                if (zbiór.Contains(pracownik))
+                {
+                    pracownik.przełożony = null;
+                    zbiór.Remove(pracownik);
+                    return true;
+                }
+                else
+                    return false;
+            }
+            public bool Contains(Pracownik pracownik)
+            {
+                return zbiór.Contains(pracownik);
+            }
+
+            bool ISet<Pracownik>.Add(Pracownik item)
+            {
+                return ((ISet<Pracownik>)Zbiór).Add(item);
+            }
+
+            public void UnionWith(IEnumerable<Pracownik> other)
+            {
+                ((ISet<Pracownik>)Zbiór).UnionWith(other);
+            }
+
+            public void IntersectWith(IEnumerable<Pracownik> other)
+            {
+                ((ISet<Pracownik>)Zbiór).IntersectWith(other);
+            }
+
+            public void ExceptWith(IEnumerable<Pracownik> other)
+            {
+                ((ISet<Pracownik>)Zbiór).ExceptWith(other);
+            }
+
+            public void SymmetricExceptWith(IEnumerable<Pracownik> other)
+            {
+                ((ISet<Pracownik>)Zbiór).SymmetricExceptWith(other);
+            }
+
+            public bool IsSubsetOf(IEnumerable<Pracownik> other)
+            {
+                return ((ISet<Pracownik>)Zbiór).IsSubsetOf(other);
+            }
+
+            public bool IsSupersetOf(IEnumerable<Pracownik> other)
+            {
+                return ((ISet<Pracownik>)Zbiór).IsSupersetOf(other);
+            }
+
+            public bool IsProperSupersetOf(IEnumerable<Pracownik> other)
+            {
+                return ((ISet<Pracownik>)Zbiór).IsProperSupersetOf(other);
+            }
+
+            public bool IsProperSubsetOf(IEnumerable<Pracownik> other)
+            {
+                return ((ISet<Pracownik>)Zbiór).IsProperSubsetOf(other);
+            }
+
+            public bool Overlaps(IEnumerable<Pracownik> other)
+            {
+                return ((ISet<Pracownik>)Zbiór).Overlaps(other);
+            }
+
+            public bool SetEquals(IEnumerable<Pracownik> other)
+            {
+                return ((ISet<Pracownik>)Zbiór).SetEquals(other);
+            }
+
+            public void Clear()
+            {
+                ((ICollection<Pracownik>)Zbiór).Clear();
+            }
+
+            public void CopyTo(Pracownik[] array, int arrayIndex)
+            {
+                ((ICollection<Pracownik>)Zbiór).CopyTo(array, arrayIndex);
+            }
+
+            public IEnumerator<Pracownik> GetEnumerator()
+            {
+                return ((IEnumerable<Pracownik>)Zbiór).GetEnumerator();
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return ((IEnumerable)Zbiór).GetEnumerator();
+            }
+
+            public ZbiórPodwładnych(Pracownik przełożony)
+            {
+                this.przełożony = przełożony;
+            }
+        }
+        public Pracownik()
+        {
+            Podwładni = new ZbiórPodwładnych(this);
+        }
+
+        public Pracownik przełożony;
+
+        public readonly ZbiórPodwładnych Podwładni;
+
+        public Pracownik BezpośredniPrzełożony // instrukcja ustawiającą nowego przełożonego [wraz z korektą]
+        {
+            get => przełożony;
+            set
+            {
+                if (przełożony != null)
+                    przełożony.Podwładni.Remove(this);
+                przełożony = value;
+                if (przełożony != null)
+                    przełożony.Podwładni.Add(this);
+            }
+        }
+
+        string
+            imię,
+            nazwisko,
+            imięNazwisko;
+
+        public string Imię // instrukcja [konstruktor] deklarująca imię
+        {
+            get
+            {
+                return imię;
+            }
+            set
+            {
+                // sprawdzanie, czy value nie jest puste
+                if (value == null)
+                    throw new Exception();
+                else
+                    imię = value;
+                sklejoneImięNazwisko();
+            }
+        }
+        public string Nazwisko // instrukcja [konstruktor] deklarująca nazwisko
+        {
+            get
+            {
+                return nazwisko;
+            }
+            set
+            {
+                if (value == null)
+                    throw new Exception();
+                else
+                    nazwisko = value;
+                sklejoneImięNazwisko();
+            }
+        }
+        public string ImięNazwisko // instrukcja deklarująca imię oraz nazwisko
+        {
+            get
+            {
+                return imięNazwisko;
+            }
+            set
+            {
+                string[] rozbicie = value.Split(' ');
+                imię = rozbicie[0];
+                if (rozbicie.Length > 1)
+                    nazwisko = rozbicie[0];
+                else
+                    nazwisko = "";
+                sklejoneImięNazwisko();
+            }
+        }
+        void sklejoneImięNazwisko() // instrukcja łącząca imię i nazwisko
+        {
+            if (nazwisko != " ")
+                imięNazwisko = imię + " " + nazwisko;
+            else
+                imięNazwisko = imię;
+        }
+
+        /*public Pracownik(string imię, string Nazwisko) // konstruktor
+        {
+            this.imię = imię;
+            this.nazwisko = Nazwisko;
+            sklejoneImięNazwisko();
+            Podwładni = new ZbiórPodwładnych(this);
+        }*/
+
+        public override string ToString() // automatyczna konwersja na stringa, gdy chcemy użyć obiekt zamiast stringa ( do przeładowania )
+        {
+            return $"{ImięNazwisko}";
+        }
+    }
+}
